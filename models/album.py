10de66c61afc1b591637f65f6e38166adf33ff8f
Cetins <69481095+Cetins@users.noreply.github.com>
2022-06-07 19:03:52 +0100

    album class has been created

---
+++ models/album.py
@@ -0,0 +1,8 @@
+class Album:
+    def __init__(self, title, year, artist, id=None):
+        self.title = title
+        self.year = year
+        self.artist = artist
+        self.id = id
+        
+    
